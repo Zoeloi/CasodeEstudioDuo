79484c4edbc20d52cc4b5b8dcaf36d7909371fdc
ValcocerLopez <vmalcocer@uamv.edu.ni>
2025-11-21 00:16:18 -0600

    Añadiendo el buscador por fechas

--- BibliotecaDigital/Servicios/BuscadorporFecha.cs
+++ BibliotecaDigital/Servicios/BuscadorporFecha.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BibliotecaDigital.Models;
+
+namespace BibliotecaDigital.Services
+{
+    public static class BuscadorFecha
+    {
+        public static void MostrarRecienteYAntiguo(List<Libro> libros)
+        {
+            var reciente = libros.OrderByDescending(l => l.Año).First();
+            var antiguo = libros.OrderBy(l => l.Año).First();
+
+            Console.WriteLine($"Más reciente: {reciente.Titulo} ({reciente.Año})");
+            Console.WriteLine($"Más antiguo: {antiguo.Titulo} ({antiguo.Año})");
+        }
+    }
+}
