a3a396d05edd651c5e2374ace51f8323df0062dd
Zoeloi <eaalmendarez@uamv.edu.ni>
2025-11-21 00:03:48 -0600

    Actualizando buscador Lineal

--- BibliotecaDigital/Servicios/BuscadorLineal.cs
+++ BibliotecaDigital/Servicios/BuscadorLineal.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BibliotecaDigital.Models;
 
-namespace BibliotecaDigital.Servicios
+namespace BibliotecaDigital.Services
 {
-    internal class BuscadorLineal
+    public static class BuscadorLineal
     {
+        public static void BuscarPorTitulo(List<Libro> libros)
+        {
+            Console.Write("Ingrese el título a buscar: ");
+            string titulo = Console.ReadLine().ToLower();
+
+            var libro = libros.FirstOrDefault(l => l.Titulo.ToLower().Contains(titulo));
+
+            if (libro == null)
+                Console.WriteLine("✗ No se encontró el libro.");
+            else
+                Console.WriteLine($"✓ Encontrado: {libro.Titulo} ({libro.Año}) - {libro.Autor}");
+        }
     }
 }
