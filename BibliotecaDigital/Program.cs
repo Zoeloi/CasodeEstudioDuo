64ec388c675453a240104e6cd34f8c52a7cd0c93
ValcocerLopez <vmalcocer@uamv.edu.ni>
2025-11-21 00:36:29 -0600

    Realizando el menu y probando el sistema

--- BibliotecaDigital/Program.cs
+++ BibliotecaDigital/Program.cs
@@ -1,15 +1,57 @@
-﻿using System;
+﻿using BibliotecaDigital.Datos;
+using BibliotecaDigital.Models;
+using BibliotecaDigital.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BibliotecaDigital
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<Libro> libros = DatosIniciales.ObtenerLibrosIniciales();
+
+
+
+        string[] autores = libros
+                            .Select(l => l.Autor)
+                            .Distinct()
+                            .OrderBy(a => a)
+                            .ToArray();
+
+        int opcion;
+
+        do
         {
-        }
+            Console.Clear();
+            Console.WriteLine("===== BIBLIOTECA DIGITAL ESTUDIANTIL =====");
+            Console.WriteLine("1. Búsqueda lineal de libro por título");
+            Console.WriteLine("2. Búsqueda binaria de autor");
+            Console.WriteLine("3. Libro más reciente y más antiguo");
+            Console.WriteLine("4. Búsqueda por coincidencias en descripción");
+            Console.WriteLine("0. Salir");
+            Console.Write("Seleccione una opción: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    BuscadorLineal.BuscarPorTitulo(libros);
+                    break;
+                case 2:
+                    BuscadorBinario.BuscarAutor(autores);
+                    break;
+                case 3:
+                    BuscadorFecha.MostrarRecienteYAntiguo(libros);
+                    break;
+                case 4:
+                    BuscadorDescripcion.BuscarCoincidencias(libros);
+                    break;
+            }
+
+            Console.WriteLine("\nPresione una tecla para continuar...");
+            Console.ReadKey();
+
+        } while (opcion != 0);
     }
 }
