158967d0bc8f7f6a6f22cde18cd7bbf00f757cd6
Zoeloi <eaalmendarez@uamv.edu.ni>
2025-11-20 23:06:18 -0600

    first commit

---
+++ BibliotecaDigital/Servicios/BuscadorBinario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDigital.Servicios
+{
+    internal class BuscadorBinario
+    {
+    }
+}
